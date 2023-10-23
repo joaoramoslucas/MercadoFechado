1350c8946b2fe3ee7ec8c48a8a7f38ecdfafd531
Joao Lucas <joaopedroramosdelucas10@gmail.com>
2023-10-23 15:38:22 -0300

    First commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components/native";
+import { SafeAreaView, Text, View } from "react-native";
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>OLÁ</Text>
+      </View>
+    </SafeAreaView>               
+  );
+}
+const style = styled.Text
+
+export default App;
